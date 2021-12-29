589cb7693fd650e94f3deeaa0b2287391628ae4c
vArcticFox <38292264+sampleExercise@users.noreply.github.com>
2021-12-29 03:23:14 -0500

    day 4

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # leetcode_exercises
+
+algo 1 https://leetcode.com/study-plan/algorithm/?progress=m1tjf0i
--- src/com/company/leetcode/algo/day4/Reverse_words_in_a_string_III.java
+++ src/com/company/leetcode/algo/day4/Reverse_words_in_a_string_III.java
@@ -4,7 +4,10 @@ package com.company.leetcode.algo.day4;
 public class Reverse_words_in_a_string_III {
     public static String reverseWords(String s) {
         String[] words = s.split(" ");
-        return "0";
+        StringBuilder result = new StringBuilder();
+        for (String word: words)
+            result.append(new StringBuffer(word).reverse().toString() + " ");
+        return result.toString().trim();
     }
 
 
